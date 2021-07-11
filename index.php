5442a9e2ad4ec23e22d44e1c8827dc518b422223
Amine <coccinellus@gmail.com>
2021-07-12 01:32:51 +0200

    version avec programme de la journée

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="src/css/normalize.css">
     <link rel="stylesheet" href="src/css/style.css">
+
     <title>Colloque 2021</title>
 </head>
 <body>
@@ -60,7 +61,7 @@
                     </table>
                     <div class="callToAction">
                         <a href="reservation.php" class="callToActionRegister">Réserver Maintenant</a>
-                        <a href="#main" class="callToActionRead">En savoir plus</a>
+                        <a href="#two" class="callToActionRead">En savoir plus</a>
                     </div>
                 </div>
             </header>
@@ -69,7 +70,7 @@
     
     <section class="main-one">
         <div class="presentation-one">
-            <p><img src="src/images/helppicture.jpg" alt="image-help"><strong>L’Affect</strong>, association créée en 1998, se trouve aux origines des réflexions sur la nécessaire formation des mandataires judiciaires et de la professionnalisation du métier. La création de l’association découle d’une réflexion sur le manque de formation des tuteurs et curateurs.
+            <p><img src="src/images/Affect-Association-Logo-Application-Color.jpg" alt="image-logo"><strong>L’Affect</strong>, association créée en 1998, se trouve aux origines des réflexions sur la nécessaire formation des mandataires judiciaires et de la professionnalisation du métier. La création de l’association découle d’une réflexion sur le manque de formation des tuteurs et curateurs.
             
 
             En 1998, un groupe de travail s’est réuni à Arcachon autour de cette problématique. Depuis 2009, le colloque annuel de l’Affect rassemble les experts du domaine, qu’ils soient juristes, mandataires, psychologues, médecins, politiques, magistrats, philosophes… Les conférenciers sont aussi bien chercheurs et universitaires d’une part, que praticiens des mesures de protection des majeurs d’autre part. Entre analyse théorique et approche pratique, le Colloque offre une large réflexion sur le métier de MJPM. Ces aller-retours entre pratiques professionnelles et normes juridiques, sociales et éthiques se trouvent au cœur même de l’activité de protection.
@@ -78,74 +79,122 @@
         </div>
     </section>
 
-    <section class="main-two">
-        <h2>Programme</h2>
-        <h2>ACCUEIL</h2>
-        <p>Georges DECOCQ</p>
-        <p>Président d'Affect</p>
-        <h2>INTRODUCTION</h2>
-        <p>Éric FIAT</p>
-        <p>Professeur à l'Université de PARIS-EST Marne la Vallée</p> 
-        <h2>I. PRÉSENTATION DES TRAVAUX SUR LES QUESTIONNEMENTS ETHIQUES DU MJPM</h2>
-        <h3 class="test">Présentation des travaux sur la charte d’orientation, état des lieux, déontologie</h3>
-        <ul class="closedList">
-            <li>Anne CARON-DÉGLISE : Magistrat au Ministère de la Justice à Paris</li>
-        </ul>
+    <section class="main-two" id="two">
+        <h3 id="programme">Programme de la journée</h2>
+        <ul class="accordion">
+            <li class="has-sub">
+                <label for="menu1"><h2>ACCUEIL</h2></label>
+                <input id="menu1" type="checkbox" name="menu"/>
+                <ul class="sub">
+                    <li>
+                        Georges DECOCQ:Président d'Affect
+                    </li>
+                </ul>
+            </li>
+            <li class="has-sub">
+                <label for="menu2"><h2>INTRODUCTION</h2></label>
+                <input id="menu2" type="checkbox" name="menu"/>
+                <ul class="sub">
+                    <li>
+                        Éric FIAT:Professeur à l'Université de PARIS-EST Marne la Vallée
+                    </li>
+                </ul>
+            </li>
+            <li class="has-sub">
+                <label for="menu3"><h2>I. PRÉSENTATION DES TRAVAUX SUR LES QUESTIONNEMENTS ETHIQUES DU MJPM</h2></label>
+                <input id="menu3" type="checkbox" name="menu"/>
+                <ul class="sub">
+                    <li><h3 class="test">Présentation des travaux sur la charte d’orientation, état des lieux, déontologie</h3>
+                        <ul>
+                            <li>Anne CARON-DÉGLISE : Magistrat au Ministère de la Justice à Paris</li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li class="has-sub">   
+                <label for="menu4"><h2>II. L'ETHIQUE DU MJPM A L'EGARD DE LA PERSONNE PROTÉGÉE</h2></label>
+                <input id="menu4" type="checkbox" name="menu"/>
+                <ul class="sub">
+                    <li><h3>Éthique du MJPM et vie de couple de la personne protégée</h3>
+                    <ul>
+                        <li>Nathalie PETERKA : Professeur et Directrice du DIU à la faculté de droit de l’Université PARIS-EST</li>
+                    </ul>
+                    </li>
 
-        <h2>II. L'ETHIQUE DU MJPM A L'EGARD DE LA PERSONNE PROTÉGÉE</h2>
-        <h3>Éthique du MJPM et vie de couple de la personne protégée</h3>
-        <ul>
-            <li>Nathalie PETERKA : Professeur et Directrice du DIU à la faculté de droit de l’Université PARIS-EST</li>
+                    <li><h3>Éthique du MJPM, gestion et transmission patrimoniale des biens</h3>
+                    <ul>
+                        <li>Jean Marie PLAZY : Professeur et responsable du Master 2 à la faculté de droit de l’Université Bordeaux</li>
+                        <li>Cécile LACRONIQUE : MJPM indépendante, Co-présidente de l’Association des mandataires Ile de France</li>
+                    </ul>
+                    </li>
+                    <li><h3>Éthique du MJPM, cadre de vie et relations personnelles de la personne protégée</h3>
+                    <ul>
+                        <li>Sylvie MOISDON-CHATAIGNER : Maitre de conférences des Universités, HDR Présidente de l’Association pour la diffusion et la connaissance du droit dans le secteur social, médico-social et éducatif</li>
+                        <li>Séverine ROY : MJPM indépendante, Présidente de la FNMJI</li>
+                    </ul>
+                    </li>
+                </ul>
+            </li>
+            <li class="has-sub">   
+                <label for="menu5"><h2>III. L'ETHIQUE DU MJPM A L'EGARD DE LA FAMILLE DE LA PERSONNE PROTEGÉE</h2></label>
+                <input id="menu5" type="checkbox" name="menu"/>
+                <ul class="sub">
+                    <li>
+                        <h3>Éthique du MJPM et vie familiale de la personne protégée : autorité parentale et majeur protégé en couple</h3>
+                        <ul>
+                            <li>Annick BATTEUR : Professeur émérite, doyen honoraire de la faculté de droit de CAEN.</li>
+                            <li>Anne GOZARD : MJPM indépendante , Présidente de la Chambre Nationale des MJPM</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Éthique du MJPM et mesure partagée : co-mesures, mesures divisées, subrogées tutelles ou curatelles</h3>
+                        <ul>
+                            <li>Gilles RAOUL CORMEIL : Professeur à l’Université de Bretagne Occidentale, responsable du Master Protection des personnes vulnérables</li>
+                            <li>Guillaume SOUTRA : MJPM indépendant</li>
+                            <li>Saskia LYSSANDRE : MJPM indépendante, co-présidente de l’association des mandataires Ile de France</li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li class="has-sub">   
+                <label for="menu6"><h2>IV. L'ETHIQUE DU MJPM A L'EGARD DES AUTRES INTERVENANTS PROFESSIONNELS</h2></label>
+                <input id="menu6" type="checkbox" name="menu"/>
+                <ul class="sub">
+                    <li>
+                        <h3>Éthique et accompagnement social</h3>
+                        <ul>
+                            <li>Ange FINISTROSA : Président de la FNAT</li>
+                            <li>Paloma MARTIN LAC : MJPM indépendante</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Éthique du MJPM et soins délivrés à la personne protégée (refus de soins, recours au juge en cas de désaccord)</h3>
+                        <ul>
+                            <li>Laurence GATTI : Maitre de conférences en droit privé et sciences criminelles, équipe de recherche en droit privé à l’Université de Poitiers</li>
+                            <li>Alexandre LEDEME : Directeur d’une association tutélaire</li>
+                            <li>Véronique LEFEBVRE DES NOETTES : Psychiatre, gériatre à l’hôpitale Émile Roux à Limeil-Brévannes, enseignant à l’Université de Paris est Creteil, membre du conseil scientifique de l’Espace national d’éthique sur les maladies</li>
+                            <li>Aude GAUTHIER : MJPM, membre du bureau national de l’ANDP</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h3>Éthique du MJPM et capacité pénale la personne protégée, auteure ou victime d’infractions</h3>
+                        <ul>
+                            <li>Claudia GHICA-LEMARCHAND : Professeure à la faculté de droit de l’Université PARIS-EST Creteil, Responsable du Master Droit pénal des affaires.</li>
+                            <li>Aline OMEZ : MJPM Déléguée responsable pôle juridique UDAF</li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li class="has-sub">
+                <label for="menu7"><h2>RAPPORT DE SYNTHÈSE</h2></label>
+                <input id="menu7" type="checkbox" name="menu"/>
+                <ul class="sub">
+                    <li>
+                        Raymond LE GUIDEC:Professeur émérite à l'Université de NANTES
+                    </li>
+                </ul>
+            </li>
         </ul>
-
-        <h3>Éthique du MJPM, gestion et transmission patrimoniale des biens</h3>
-        <ul>
-            <li>Jean Marie PLAZY : Professeur et responsable du Master 2 à la faculté de droit de l’Université Bordeaux</li>
-            <li>Cécile LACRONIQUE : MJPM indépendante, Co-présidente de l’Association des mandataires Ile de France</li>
-        </ul>
-
-
-        <h3>Éthique du MJPM, cadre de vie et relations personnelles de la personne protégée</h3>
-        <ul>
-            <li>Sylvie MOISDON-CHATAIGNER : Maitre de conférences des Universités, HDR Présidente de l’Association pour la diffusion et la connaissance du droit dans le secteur social, médico-social et éducatif</li>
-            <li>Séverine ROY : MJPM indépendante, Présidente de la FNMJI</li>
-        </ul>
-        <h2>III. L'ETHIQUE DU MJPM A L'EGARD DE LA FAMILLE DE LA PERSONNE PROTEGÉE</h2>
-        <h3>Éthique du MJPM et vie familiale de la personne protégée : autorité parentale et majeur protégé en couple</h3>
-        <ul>
-            <li>Annick BATTEUR : Professeur émérite, doyen honoraire de la faculté de droit de CAEN.</li>
-            <li>Anne GOZARD : MJPM indépendante , Présidente de la Chambre Nationale des MJPM</li>
-        </ul>
-
-        <h3>Éthique du MJPM et mesure partagée : co-mesures, mesures divisées, subrogées tutelles ou curatelles</h3>
-        <ul>
-            <li>Gilles RAOUL CORMEIL : Professeur à l’Université de Bretagne Occidentale, responsable du Master Protection des personnes vulnérables</li>
-            <li>Guillaume SOUTRA : MJPM indépendant</li>
-            <li>Saskia LYSSANDRE : MJPM indépendante, co-présidente de l’association des mandataires Ile de France</li>
-        </ul>
-
-        <h2>IV. L'ETHIQUE DU MJPM A L'EGARD DES AUTRES INTERVENANTS PROFESSIONNELS</h2>
-        <h3>Éthique et accompagnement social</h3>
-        <ul>
-            <li>Ange FINISTROSA : Président de la FNAT</li>
-            <li>Paloma MARTIN LAC : MJPM indépendante</li>
-        </ul>
-        <h3>Éthique du MJPM et soins délivrés à la personne protégée (refus de soins, recours au juge en cas de désaccord)</h3>
-        <ul>
-            <li>Laurence GATTI : Maitre de conférences en droit privé et sciences criminelles, équipe de recherche en droit privé à l’Université de Poitiers</li>
-            <li>Alexandre LEDEME : Directeur d’une association tutélaire</li>
-            <li>Véronique LEFEBVRE DES NOETTES : Psychiatre, gériatre à l’hôpitale Émile Roux à Limeil-Brévannes, enseignant à l’Université de Paris est Creteil, membre du conseil scientifique de l’Espace national d’éthique sur les maladies</li>
-            <li>Aude GAUTHIER : MJPM, membre du bureau national de l’ANDP</li>
-        </ul>
-        <h3>Éthique du MJPM et capacité pénale la personne protégée, auteure ou victime d’infractions</h3>
-        <ul>
-            <li>Claudia GHICA-LEMARCHAND : Professeure à la faculté de droit de l’Université PARIS-EST Creteil, Responsable du Master Droit pénal des affaires.</li>
-            <li>Aline OMEZ : MJPM Déléguée responsable pôle juridique UDAF</li>
-        </ul>
-
-        <h2>RAPPORT DE SYNTHÈSE</h2>
-        <p>Raymond LE GUIDEC</p>
-        <p>Professeur émérite à l'Université de NANTES</p>
     </section>
     
     <footer>
